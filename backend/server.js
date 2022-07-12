657882e037945052fcc6fc25e2dd9e32804f9350
Robbie Prokop <robbieprokop@gmail.com>
2022-07-12 16:57:35 -0600

    created middleware to handle errors

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -16,6 +18,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(
   PORT,
