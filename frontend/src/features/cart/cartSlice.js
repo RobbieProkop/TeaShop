8213df037c8215dd14e311c4f71192a056abe321
Robbie Prokop <robbieprokop@gmail.com>
2022-07-26 22:50:18 -0600

    begin reworking on cartSlice

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -1,64 +1,83 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = {
-  cartItems: [],
-  isError: false,
-  isSuccess: false,
-  isLoading: false,
-  message: "",
-};
+// const initialState = {
+//   cartItems: [],
+//   isError: false,
+//   isSuccess: false,
+//   isLoading: false,
+//   message: "",
+// };
 
-export const addCartItems = createAsyncThunk(
-  "cart/addCartItems",
-  async (productId, thunkAPI) => {
-    try {
-      const existItem = state.cartItems.find((x) => x.product === item.product);
+// export const addCartItems = createAsyncThunk(
+//   "cart/addCartItems",
+//   async (productId, thunkAPI) => {
+//     try {
+//       const existItem = state.cartItems.find((x) => x.product === item.product);
 
-      if (!existItem) {
-        return {
-          ...state,
-          cartItems: [...state.cartItems, item],
-        };
-      }
-      return {
-        ...state,
-        cartItems: state.cartItems.map((x) =>
-          x.product === existItem.product ? item : x
-        ),
-      };
-    } catch (error) {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
-);
+//       if (!existItem) {
+//         return {
+//           ...state,
+//           cartItems: [...state.cartItems, item],
+//         };
+//       }
+//       return {
+//         ...state,
+//         cartItems: state.cartItems.map((x) =>
+//           x.product === existItem.product ? item : x
+//         ),
+//       };
+//     } catch (error) {
+//       const message =
+//         (error.response &&
+//           error.response.data &&
+//           error.response.data.message) ||
+//         error.message ||
+//         error.toString();
+//       return thunkAPI.rejectWithValue(message);
+//     }
+//   }
+// );
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     cartReducer: (state = {cartItems: []}, action => {
+
+//     })
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(addCartItems.pending, (state) => {
+//         state.isLoading = true;
+//         state = { ...state };
+//       })
+//       .addCase(addCartItems.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.isSuccess = true;
+//         state.cartItems = action.payload;
+//       })
+//       .addCase(addCartItems.rejected, (state, action) => {
+//         state.isLoading = false;
+//         state.isError = true;
+//         state.message = action.payload;
+//       });
+//   },
+// });
+
+// export default cartSlice.reducer;
+
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(addCartItems.pending, (state) => {
-        state.isLoading = true;
-        state = { ...state };
-      })
-      .addCase(addCartItems.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = true;
-        state.cartItems = action.payload;
-      })
-      .addCase(addCartItems.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = true;
-        state.message = action.payload;
-      });
+  initialState: {
+    itemsList: [],
+    qty: 0,
+    showCart: false,
+  },
+  reducers: {
+    addToCart() {},
+    removeFrom,
   },
 });
-
-export default cartSlice.reducer;
